82af7311b89b16f94c1cc0491ccbfc1ac004704e
oncemore <oncemoreforyou@outlook.com>
2017-07-16 05:18:00 +0300

    init commit
    init commit

---
+++ QRCodeReader/QRCodeViewController.swift
@@ -0,0 +1,32 @@
+//
+//  QRCodeViewController.swift
+//  QRCodeReader
+//
+//  Created by Mobile Dev on 13/7/2017.
+//  Copyright © 2017 Mobile Dev. All rights reserved.
+//
+
+import UIKit
+
+class QRCodeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    
+    // MARK: - Navigation
+
+    @IBAction func unwindToHomeScreen(segue: UIStoryboardSegue) {
+        dismiss(animated: true, completion: nil)
+    }
+
+}
